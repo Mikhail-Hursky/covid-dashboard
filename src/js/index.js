5d61772041c67e6ee8603b93b32b575105ec1b75
Yuliya Kryvorot <krviua@yahoo.com>
2020-12-11 22:01:01 +0300

    fix: implement correct loading data from covid api

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import '../styles/style.css';
+
 export default class Hee {
   init() {}
 }
@@ -6,8 +8,8 @@ function getCountryFlag(countryCode) {
   return `https://www.countryflags.io/${countryCode}/flat/32.png`;
 }
 
-async function getListOfCountries() {
-  const URL = 'https://api.covid19api.com/countries';
+async function getTotalCases() {
+  const URL = 'https://api.covid19api.com/summary';
   try {
     const response = await fetch(URL);
     const data = await response.json();
@@ -16,30 +18,3 @@ async function getListOfCountries() {
     throw new Error(e);
   }
 }
-
-/**
- * @param {string} country
- * @param {string} date
- *
- * @example
- *   'belarus'
- *   '2020-12-08T00:00:00Z'
- *   '2020-12-09T00:00:00Z'
- */
-
-async function getTotalCasesByCountry(country, startDate, endDate) {
-  const URL = `https://api.covid19api.com/total/country/${country}/status/confirmed?from=${startDate}&to=${endDate}`;
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    return data[data.length - 1]['Cases'];
-  } catch (e) {
-    throw new Error(e);
-  }
-}
-
-getTotalCasesByCountry('belarus', '2020-12-08T00:00:00Z', '2020-12-10T00:00:00Z').then(cases => {
-  console.log(cases);
-  console.log(getCountryFlag('BS'));
-});
-getListOfCountries(); // 248 countries
