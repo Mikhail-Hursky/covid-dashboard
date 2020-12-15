bf027200a99cb0cecc992930417f3c22c74f03cd
Mikhail Hursky <kakai747o@gmail.com>
2020-12-15 18:55:04 +0300

    feat: add map

--- src/js/application/api/Api.js
+++ src/js/application/api/Api.js
@@ -1,6 +1,8 @@
 export default class Api {
   constructor() {
     this.covidUrl = 'https://api.covid19api.com/';
+    this.locationUrl = 'https://api.positionstack.com/v1/forward';
+    this.apiKeyPositionstack = 'e157c7336d0add5ad9ecb1214f2a8d72';
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -17,4 +19,7 @@ export default class Api {
       throw new Error(e);
     }
   }
+
+  // eslint-disable-next-line no-unused-vars
+  async getLatLon(country) {}
 }
