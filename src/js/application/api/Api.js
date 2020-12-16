97cae31c0574f5e216e0ce39e6eb79117e87e438
Yuliya Kryvorot <krviua@yahoo.com>
2020-12-16 12:08:20 +0300

    feat: add api key

--- src/js/application/api/Api.js
+++ src/js/application/api/Api.js
@@ -1,6 +1,7 @@
 export default class Api {
   constructor() {
-    this.covidUrl = 'https://api.covid19api.com/';
+    this.covidURL = 'https://api.covid19api.com/';
+    this.KEY = '5cf9dfd5-3449-485e-b5ae-70a60e997864';
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -10,7 +11,11 @@ export default class Api {
 
   async getCovidData() {
     try {
-      const response = await fetch(`${this.covidUrl}summary`);
+      const response = await fetch(`${this.covidURL}summary`, {
+        headers: {
+          'X-Access-Token': this.KEY,
+        },
+      });
       const data = await response.json();
       return data;
     } catch (e) {
