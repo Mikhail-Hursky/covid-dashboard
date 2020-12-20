d50e787f0564aef259d54cf0888042ad149503a3
Mikhail Hursky <kakai747o@gmail.com>
2020-12-20 19:43:35 +0300

    feat: implement map and chart

--- src/js/application/preload/Preload.js
+++ src/js/application/preload/Preload.js
@@ -7,15 +7,21 @@ export default class Preload {
 
   init() {
     this.container = new ElementBuilder('div', 'preload');
-    const img = new ElementBuilder('img');
+    this.img = new ElementBuilder('img');
     // eslint-disable-next-line
-    img.element.src =
+    this.img.element.src =
       'https://cdn.dribbble.com/users/846207/screenshots/5568468/gradient-circle-loading.gif';
-    this.container.append(img);
+    this.container.append(this.img);
     this.container.appendToBody();
   }
 
   removePreload() {
     this.container.element.remove();
+  }
+
+  errorServer() {
+    this.img.remove();
+    this.h1 = new ElementBuilder('h1');
+    this.h1.element.textContent = 'Fill the server with new data. Come back later.';
   }
 }
